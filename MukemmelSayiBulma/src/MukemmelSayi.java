1c1242fe41fc5533de8fe6cbbfd2aa734cd7938f
Ruhi Mert Aksu <96263914+mrtaksuuu@users.noreply.github.com>
2022-04-27 05:10:19 +0300

    Add files via upload

---
+++ MukemmelSayiBulma/src/MukemmelSayi.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class MukemmelSayi {
+    public static void main(String[] args){
+
+        Scanner inp=new Scanner(System.in);
+        int toplam = 0;
+
+        System.out.print("Bir sayi giriniz: ");
+        int sayi = inp.nextInt();
+
+        for(int i = 1; i < sayi; i++)
+        {
+            if(sayi % i == 0) {
+                toplam += i;
+            }
+        }
+        if(sayi == toplam) {
+            System.out.println(sayi + " Mukemmel bir sayidir");
+        }
+        else {
+            System.out.println(sayi + " Mukemmel bir sayi degildir.");
+        }
+
+
+
+    }
+}
