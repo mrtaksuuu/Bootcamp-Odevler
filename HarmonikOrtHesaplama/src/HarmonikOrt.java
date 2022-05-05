aa0a40a569a2cb736c48ba660638045f0408a9b5
Ruhi Mert Aksu <96263914+mrtaksuuu@users.noreply.github.com>
2022-05-05 14:58:03 +0300

    Add files via upload

---
+++ HarmonikOrtHesaplama/src/HarmonikOrt.java
@@ -0,0 +1,17 @@
+public class HarmonikOrt {
+    public static void main(String[] args) {
+
+        double[] liste = {1, 2, 3, 4, 5, 6, 7, 8};
+        double harmonikSeriToplami = 0.0;
+        double harmonikSeriOrtalamasi;
+
+        for (double i : liste) {
+            harmonikSeriToplami += (1 / i);
+        }
+        harmonikSeriOrtalamasi = liste.length / harmonikSeriToplami;
+
+        System.out.println("Harmonik Seri Toplamı\t: " + harmonikSeriToplami);
+        System.out.println("Harmonik Seri Ortalaması: " + harmonikSeriOrtalamasi);
+
+    }
+}
